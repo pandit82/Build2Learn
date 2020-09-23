495398cdd80ba34ea48d525bf563612c1e81b146
pandit82 <46365232+pandit82@users.noreply.github.com>
2020-09-23 00:06:37 -0400

    updated

---
+++ restful-web-services/src/main/java/com/learning/webservice/basic/auth/BasicAuthenticationController.java
@@ -0,0 +1,16 @@
+package com.learning.webservice.basic.auth;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@CrossOrigin(origins="http://localhost:4200")
+@RestController
+public class BasicAuthenticationController {
+	
+	@GetMapping("/basicauth")
+	public AuthenticationBean authenticationBean() {
+		//throw new RuntimeException("Error occered please contact support 800-299-3884");
+		return new AuthenticationBean("You are Successfully authenticated");
+	}
+	
+}
